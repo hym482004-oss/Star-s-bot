a317af53dbefbaa307d579278c6bb40c29c0a2fb
hym482004-oss <hym482004@gmail.com>
2026-05-07 12:35:33 +0630

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import asyncio
 import os
 from dotenv import load_dotenv
 
+from parser import parse_message
+
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
@@ -12,9 +14,21 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+
 @dp.message(CommandStart())
 async def start(message: Message):
     await message.reply("2D Bot စပြီး run နေပြီ ✅")
+
+
+@dp.message()
+async def handle(message: Message):
+    data = parse_message(message.text)
+
+    await message.reply(
+        f"📦 RAW:\n{data['raw']}\n\n"
+        f"📄 LINES:\n{data['lines']}"
+    )
+
 
 async def main():
     await dp.start_polling(bot)
